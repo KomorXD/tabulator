a693cda33b2490c18bf62521cf61e5913b812f20
Paweł Kupczak <pawel.kupczak.sc@gmail.com>
2023-04-20 21:02:00 +0200

    Add DB context

---
+++ tabulator/guwno/DBContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tabulator.Models;
+
+namespace tabulator.guwno
+{
+    internal class DBContext : DbContext
+    {
+        static public DBContext INSTANCE = null;
+
+        private DBContext(): base(Properties.Settings.Default.db_connect_str)
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new FacultyConfiguration());
+        }
+
+        public DbSet<Faculty> Faculties { get; set; }
+
+        static public DBContext GetInstance()
+        {
+            if(INSTANCE == null)
+            {
+                INSTANCE = new DBContext();
+            }
+
+            return INSTANCE;
+        }
+    }
+}
