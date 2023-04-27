967867c598ac9edda650379a5ad7aa94949be47a
Paweł Kupczak <pawel.kupczak.sc@gmail.com>
2023-04-27 22:08:16 +0200

    Add eq employees M-T-M relation

---
+++ tabulator/Models/EquipmentCaretakers.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace tabulator.Models
+{
+    [Table("EquipmentCaretakers")]
+    public class EquipmentCaretakers
+    {
+        [Key]
+        [Column(Order = 1)]
+        [ForeignKey("Employee")]
+        public int EmployeeId { get; set; }
+
+        [Key]
+        [Column(Order = 2)]
+        [ForeignKey("Item")]
+        public int EquipmentId { get; set; }
+
+        public virtual Employee Employee { get; set; }
+
+        public virtual EquipmentItem Item { get; set; }
+    }
+}
