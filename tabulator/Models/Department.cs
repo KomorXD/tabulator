c21abffcc17ba6774bd8fd0d2ee7100ba5906654
Paweł Kupczak <pawel.kupczak.sc@gmail.com>
2023-04-27 21:26:57 +0200

    Add department mapping class

---
+++ tabulator/Models/Department.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace tabulator.Models
+{
+    [Table("Department")]
+    public class Department
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Column]
+        public string Name { get; set; }
+
+        [Column]
+        public string Address { get; set; }
+
+        [ForeignKey("Faculty")]
+        public int FacultyId { get; set; }
+
+        [InverseProperty("Departments")]
+        public virtual Faculty Faculty { get; set; }
+
+        [ForeignKey("DepartmentId")]
+        public virtual ICollection<DepartmentRoom> DepartmentRooms { get; set; }
+    }
+}
