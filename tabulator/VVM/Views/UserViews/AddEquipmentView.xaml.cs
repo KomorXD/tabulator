e68d5ec48fad126996263276166f5c983eb3b102
lilboisoul <kdusza2001@gmail.com>
2023-05-11 13:27:20 +0200

    Create AddEquipmentView.xaml.cs

--- tabulator/VVM/Views/UserViews/AddEquipmentView.xaml.cs
+++ tabulator/VVM/Views/UserViews/AddEquipmentView.xaml.cs
@@ -12,16 +12,42 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
+
+
 namespace tabulator.VVM.Views.UserViews
 {
+   
     /// <summary>
     /// Interaction logic for AddEquipmentView.xaml
     /// </summary>
     public partial class AddEquipmentView : UserControl
     {
+      
         public AddEquipmentView()
         {
             InitializeComponent();
+            RoomDropdownSelection(null, null);
+        }
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            string name =        NameInput.Text;
+            string description = DescriptionInput.Text;
+            bool isAvailable =   StringToBool(AvailableInput.Text);
+            bool isInUse =       StringToBool(NotInUseInput.Text);
+            bool isDestroyed =   StringToBool(DestroyedInput.Text);
+            string selectedRoom = ((ComboBoxItem)RoomDropdown.SelectedItem)?.Content?.ToString();
+        }
+
+        private void RoomDropdownSelection(object sender, SelectionChangedEventArgs e)
+        {
+            RoomDropdown.Items.Add(new ComboBoxItem() { Content = "Room Example 1" });
+            RoomDropdown.Items.Add(new ComboBoxItem() { Content = "Room Example 2" });
+            RoomDropdown.SelectedIndex = 0;
+        }
+        private bool StringToBool(string str)
+        {
+            if (str == "True") return true;
+            return false;
         }
     }
 }
