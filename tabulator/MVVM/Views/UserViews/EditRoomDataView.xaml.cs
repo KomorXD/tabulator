21d0d87e2345f2f97788cd49ea1fdef0a9becb5e
lilboisoul <kdusza2001@gmail.com>
2023-05-23 20:11:03 +0200

    Add edit room view

--- tabulator/MVVM/Views/UserViews/EditRoomDataView.xaml.cs
+++ tabulator/MVVM/Views/UserViews/EditRoomDataView.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using tabulator.DatabaseContext;
+using tabulator.MVVM.Models;
 
 namespace tabulator.MVVM.Views.UserViews
 {
@@ -19,9 +21,34 @@ namespace tabulator.MVVM.Views.UserViews
     /// </summary>
     public partial class EditRoomDataView : UserControl
     {
+        DBContext context = DBContext.GetInstance();
+        public static DataGrid dataGrid;
         public EditRoomDataView()
         {
             InitializeComponent();
+            RoomDataGrid.ItemsSource = context.Rooms.ToList();
+            dataGrid = RoomDataGrid;
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            int ID = (RoomDataGrid.SelectedItem as Room).Id;
+            EditRoomPopup editRoom = new EditRoomPopup(ID);
+            editRoom.ShowDialog();
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            int ID = (RoomDataGrid.SelectedItem as Room).Id;
+            var deleteRoom = context.Rooms.Where(m => m.Id == ID).Single();
+            context.Rooms.Remove(deleteRoom);
+            context.SaveChanges();
+            RoomDataGrid.ItemsSource = context.Rooms.ToList();
+        }
+        private void SearchTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var result = context.Rooms.Where(x => x.Number.ToString().Contains(SearchTextBox.Text)).ToList();
+            RoomDataGrid.ItemsSource = result;
         }
     }
 }
---
+++ tabulator/MVVM/Views/UserViews/EditRoomPopup.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using tabulator.DatabaseContext;
+using tabulator.MVVM.Models;
+
+namespace tabulator.MVVM.Views.UserViews
+{
+    /// <summary>
+    /// Interaction logic for EditRoomPopup.xaml
+    /// </summary>
+    public partial class EditRoomPopup : Window
+    {
+        DBContext context = DBContext.GetInstance();
+        int Id;
+
+        public EditRoomPopup(int RoomID)
+        {
+            InitializeComponent();
+            RoomTypeDropdownSelectionChanged(null, null);
+            Id = RoomID;
+
+            var editRoom = (from m in context.Rooms where m.Id == RoomID select m).FirstOrDefault();
+            RoomNumberInput.Text = editRoom.Number;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            Room room = (from m in context.Rooms where m.Id == Id select m).FirstOrDefault();
+            room.Number = RoomNumberInput.Text;
+            context.SaveChanges();
+            EditRoomDataView.dataGrid.ItemsSource = context.Rooms.ToList();
+            this.Close();
+        }
+
+        private void RoomTypeDropdownSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedRoomType = ((ComboBoxItem)RoomTypeDropdown.SelectedItem)?.Content?.ToString();
+            switch (selectedRoomType)
+            {
+                case "Faculty":
+                    RoomTypeText.Text = "Faculty";
+                    RoomTypeItemDropdown.Items.Clear();
+                    //zastapic uzupelniaime danych z bazy danych xd
+                    RoomTypeItemDropdown.Items.Add(new ComboBoxItem() { Content = "Faculty Example 1" });
+                    RoomTypeItemDropdown.Items.Add(new ComboBoxItem() { Content = "Faculty Example 2" });
+                    RoomTypeItemDropdown.SelectedIndex = 0;
+                    break;
+
+                case "Department":
+                    RoomTypeText.Text = "Department";
+                    RoomTypeItemDropdown.Items.Clear();
+                    //zastapic uzupelniaime danych z bazy danych xd
+                    RoomTypeItemDropdown.Items.Add(new ComboBoxItem() { Content = "Department Example 1" });
+                    RoomTypeItemDropdown.Items.Add(new ComboBoxItem() { Content = "Department Example 2" });
+                    RoomTypeItemDropdown.SelectedIndex = 0;
+                    break;
+            }
+        }
+    }
+}
