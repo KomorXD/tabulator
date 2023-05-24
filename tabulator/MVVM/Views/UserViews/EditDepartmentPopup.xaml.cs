e6db5e7a4c589ace174fe8a1d0453d57da454f90
Bartłomiej Gordon <bartekk.gordon@gmail.com>
2023-05-24 19:43:25 +0200

    Fill out Combobox in Department popup

--- tabulator/MVVM/Views/UserViews/EditDepartmentPopup.xaml.cs
+++ tabulator/MVVM/Views/UserViews/EditDepartmentPopup.xaml.cs
@@ -23,17 +23,20 @@ namespace tabulator.MVVM.Views.UserViews
     {
         DBContext context = DBContext.GetInstance();
         int Id;
-
+        List<Faculty> _facultyList;
         public EditDepartmentPopup(int DepartmentID)
         {
             InitializeComponent();
             Id = DepartmentID;
 
-            var editDepartment = (from m in context.Departments where m.Id == DepartmentID select m).FirstOrDefault();
+            Department editDepartment = (from m in context.Departments where m.Id == DepartmentID select m).FirstOrDefault();
             NameInput.Text = editDepartment.Name;
-            FacultyDropdown.Items.Add(new ComboBoxItem() { Content = "Faculty Example 1" });
-            FacultyDropdown.Items.Add(new ComboBoxItem() { Content = "Faculty Example 2" });
-            FacultyDropdown.SelectedIndex = 0;
+            _facultyList = context.Faculties.ToList();
+            foreach (Faculty faculty in _facultyList)
+            {
+                FacultyDropdown.Items.Add(new ComboBoxItem() { Content = faculty.Name });
+            }
+            FacultyDropdown.SelectedIndex = GetCurrentFacultyIndex(editDepartment);
         }
 
         private void btnClose_Click(object sender, RoutedEventArgs e)
@@ -49,10 +52,27 @@ namespace tabulator.MVVM.Views.UserViews
             }
         }
 
+        int GetCurrentFacultyIndex(Department dep)
+        {
+            for (int i = 0; i < _facultyList.Count; i++)
+            {
+                if (dep.Faculty.Id.Equals(_facultyList.ElementAt(i).Id))
+                    return i;
+            }
+
+            return 0;
+        }
+
         private void btnEdit_Click(object sender, RoutedEventArgs e)
         {
             Department department = (from m in context.Departments where m.Id == Id select m).FirstOrDefault();
             department.Name = NameInput.Text;
+            int selectedRole = FacultyDropdown.SelectedIndex;
+            if(selectedRole == -1)
+            {
+                return;
+            }
+            department.Faculty = _facultyList.ElementAt(selectedRole);
             context.SaveChanges();
             EditDepartmentView.dataGrid.ItemsSource = context.Departments.ToList();
             this.Close();
