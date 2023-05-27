670aa95d96a7c0107e88391f70ae984c3a36b02c
MikolajGajos <mikus22500@gmail.com>
2023-05-27 17:30:07 +0200

    Add data to employee data grid

--- tabulator/MVVM/Views/UserViews/EditEmployeeDataView.xaml.cs
+++ tabulator/MVVM/Views/UserViews/EditEmployeeDataView.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,6 +13,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using tabulator.DatabaseContext;
+using tabulator.MVVM.Models;
 
 namespace tabulator.MVVM.Views.UserViews
 {
@@ -19,9 +23,12 @@ namespace tabulator.MVVM.Views.UserViews
     /// </summary>
     public partial class EditEmployeeDataView : UserControl
     {
+        DBContext context = DBContext.GetInstance();
+
         public EditEmployeeDataView()
         {
             InitializeComponent();
+            AddDataToDataGrid(context.Employees.ToList());
         }
 
         private void BtnEdit_Click(object sender, RoutedEventArgs e)
@@ -41,7 +48,19 @@ namespace tabulator.MVVM.Views.UserViews
 
         private void SearchTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
+            var result = context.Employees.Where(x => x.Name.Contains(SearchTextBox.Text) || x.Surname.ToString().Contains(SearchTextBox.Text) || x.PESEL.ToString().Contains(SearchTextBox.Text) || x.PhoneNumber.ToString().Contains(SearchTextBox.Text)).ToList();
+            AddDataToDataGrid(result);
+        }
 
+        private void AddDataToDataGrid(List<Employee> emplList)
+        {
+            EmployeeDataGrid.ItemsSource = emplList.Select(empl => new
+            {
+                empl.Name,
+                empl.Surname,
+                empl.PESEL,
+                empl.PhoneNumber
+            }).ToList();
         }
     }
 }
