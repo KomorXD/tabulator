6a48da1fcb896ad9c827f1b11b08b854dacfb525
MikolajGajos <mikus22500@gmail.com>
2023-05-28 23:17:42 +0200

    Make search box work in EmployeeFunction

--- tabulator/MVVM/Views/UserViews/EmployeeViews/EmployeeFunction.xaml.cs
+++ tabulator/MVVM/Views/UserViews/EmployeeViews/EmployeeFunction.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using tabulator.Core;
 using tabulator.DatabaseContext;
+using tabulator.MVVM.Models;
 
 namespace tabulator.MVVM.Views.UserViews.EmployeeViews
 {
@@ -42,12 +43,32 @@ namespace tabulator.MVVM.Views.UserViews.EmployeeViews
 
         private void SearchTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-
+            List<DepartmentTechEmployee> depResult = context.DepartmentTechEmployee.ToList();
+            if (!SearchTextBox.Text.Equals(string.Empty))
+            {
+                depResult = context.DepartmentTechEmployee.Where(dep => dep.Department.Name.ToLower().Contains(SearchTextBox.Text.ToLower())).ToList();
+            }
+            List<FacultyTechEmployee> facResult = context.FacultyTechEmployee.ToList();
+            if (!SearchTextBox.Text.Equals(string.Empty))
+            {
+                facResult = context.FacultyTechEmployee.Where(fac => fac.Faculty.Name.ToLower().Contains(SearchTextBox.Text.ToLower())).ToList();
+            }
+            List<Employee> empResult = context.Employees.ToList();
+            if (!SearchTextBox.Text.Equals(string.Empty))
+            {
+                empResult = context.Employees.Where(emp => emp.Name.ToLower().Contains(SearchTextBox.Text.ToLower()) || emp.Surname.ToLower().Contains(SearchTextBox.Text.ToLower())).ToList();
+            }
+            DataGridManager.GetInstance().ShowEmployeeFunction(FunctionsDataGrid, facResult, depResult, empResult);
         }
 
         private void BtnEdit_Click(object sender, RoutedEventArgs e)
         {
-
+            var selectedItem = (dynamic)FunctionsDataGrid.SelectedItem;
+            int id = selectedItem.Id;
+            Employee employee = context.Employees.FirstOrDefault(x => x.Id == id);
+            EmployeeFunctionPopup editEmployee = new EmployeeFunctionPopup(employee);
+            editEmployee.ShowDialog();
+            DataGridManager.GetInstance().ShowEmployeeFunction(FunctionsDataGrid, context.FacultyTechEmployee.ToList(), context.DepartmentTechEmployee.ToList(), context.Employees.ToList());
         }
     }
 }
